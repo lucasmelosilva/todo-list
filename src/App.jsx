cbcb621886969bc8422f332b5a50fbe70d237e8b
Lucas Melo <lucas.melo044@gmail.com>
2022-05-05 13:01:13 -0300

    add componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 import "./main.css"
 
 function App() {
 
   return (
-    <Header />
+    <>
+      <Header />
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export function Footer() {
+  return (
+    <footer className="w-full fixed  bottom-0 px-6 py-4 md:px-8 bg-zinc-200 flex  flex-col items-center ">
+      <p className="text-zinc-400">Feito com ♥ por Lucas</p>
+      <a
+        traget="_blank"
+        href="https://pt.vecteezy.com/vetor-gratis/ilustra%C3%A7%C3%A3o"
+        className="text-zinc-400"
+      >Ilustração Vetores
+        │ por Vecteezy</a>
+
+    </footer>
+  )
+}
