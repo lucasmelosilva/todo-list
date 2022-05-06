2b9fa6706a139287414cc5b57543cf318c182d29
lucas melo <lucas.melo044@gmail.com>
2022-05-06 15:43:53 -0300

    add duas paletas de cores dark e light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: "#162447",
+          light: "#F0ECE3"
+        },
+        secondary: {
+          dark: "#1F4068",
+          light:  "#DFD3C3"
+        }        
+      }
+    },
   },
   plugins: [],
 }
