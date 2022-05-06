8baa03c60e98aa673361b08112af59fde184890e
lucas melo <lucas.melo044@gmail.com>
2022-05-06 16:05:25 -0300

    add botao do tema de cores

---
+++ src/components/ThemeButton.jsx
@@ -0,0 +1,9 @@
+import { MoonStars } from "phosphor-react"
+
+export function ThemeButton() {
+  return (
+    <button>
+      <MoonStars size="30px" weight="thin" className="text-zinc-700 hover:text-zinc-900"/>
+    </button>
+  )
+}
