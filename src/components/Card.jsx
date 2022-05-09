b5cf47b82199fc2d5f142444789b11d0becabaa8
lucas melo <lucas.melo044@gmail.com>
2022-05-08 21:23:10 -0300

    add componente ListCards && Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import { Trash } from 'phosphor-react'
+
+export function Card({ title, content }) {
+  return (
+    <div className='w-52 bg-yellow-300 rounded-xl p-2'>
+      <div className='flex items-center justify-between mb-3'>
+        <h2>{title}</h2>
+
+        <button>
+          <Trash />
+        </button>
+      </div>
+
+      <p>{
+        content.length < 100 ? content : content.substring(0, 100) + "..."
+      } </p>
+    </div>
+  )
+}
