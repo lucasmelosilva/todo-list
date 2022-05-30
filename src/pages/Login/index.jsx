a3716d8a449a19156699d561957488f0e565c50c
lucas melo <lucas.melo044@gmail.com>
2022-05-29 22:49:55 -0300

    add estilizacao na pagina de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,19 +3,19 @@ import { GithubLogo, GoogleLogo } from "phosphor-react";
 function Login() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="md:w-2/6 w-full md:h-auto h-screen md:pt-12 md:pb-12 bg-gray-800 flex flex-col items-center md:justify-between justify-center">
-        <h1>Login</h1>
+      <div className="md:w-2/6 w-full md:h-auto h-screen md:pt-12 md:pb-12 bg-gray-800 md:rounded-lg flex flex-col items-center md:justify-between justify-center">
+        <h1 className="text-slate-400 font-bold text-4xl pb-14">Faça seu login</h1>
 
-        <button>
-          Login with
-          <GoogleLogo />
+        <button className="bg-slate-700 text-xl text-slate-400 font-bold p-3 rounded-md w-64 flex justify-center items-center">
+          Entrar com
+          <GoogleLogo weight="bold" className="ml-4" />
         </button>
 
-        <p>ou</p>
+        <p className="text-gray-600 mt-3 mb-3">ou</p>
 
-        <button>
-          Login with
-          <GithubLogo />
+        <button className="bg-slate-700 text-xl text-slate-400 font-bold p-3 rounded-md w-64 flex justify-center items-center">
+          Entrar com
+          <GithubLogo weight="bold" className="ml-4" />
         </button>
       </div>
     </div>
