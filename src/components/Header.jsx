3b0ececbfa4f8b10b5f6e5ebb4b2124db731f12f
lucas melo <lucas.melo044@gmail.com>
2022-06-21 11:13:58 -0300

    novo layout Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,18 @@ import { SignOut, UserCircle } from "phosphor-react";
 
 export function Header() {
   return (
-    <header className="w-full px-6 py-4 md:px-8 bg-gray-800 flex fixed top-0 items-center justify-between">
-      <div className="w-full px-6 py-4 md:px-8 bg-gray-800 flex fixed top-0 items-center justify-between">
+    <header className="w-full py-4 bg-gray-800 flex fixed top-0 items-center justify-center">
+      <div className="w-[90%] md:w-[80%]  bg-gray-800 flex items-center justify-between">
         <div className="flex items-center">
           <UserCircle size="48px" weight="thin" className="text-slate-400" />
 
           <span className="ml-3 text-slate-400">Lucas</span>
         </div>
 
-        <div className="flex items-center">
-          <button className="ml-11">
-            <SignOut size="30px" weight="thin" className="text-slate-400 hover:text-slate-500  transition-colors" />
-          </button>
-        </div>
+        <button className="ml-11">
+          <SignOut size="30px" weight="thin" className="text-slate-400 hover:text-slate-500  transition-colors" />
+        </button>
+
       </div>
     </header>
   )
